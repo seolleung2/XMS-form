bd06bdf304d02536b241577da18554f6149696c9
seolleung2 <seolleung22@gmail.com>
2023-03-26 13:12:20 +0900

    add: BaseLayout 컴포넌트 생성 (+ Header) - 컴포넌트 감싸는 BaseLayout 컴포넌트 생성 및 적용 - Header 컴포넌트 생성 및 적용

---
+++ src/components/layout/header/index.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from 'react';
+
+const Header: FunctionComponent = () => {
+  return (
+    <header className="border-0 border-b-2 border-solid border-b-primary p-2.5">
+      <span className="text-2xl font-bold">
+        📑 Kwangmook JUNG - XMS Application
+      </span>
+    </header>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       colors: {
         light: '#F5F5F5',
+        black: '#000',
+        dark: '#191a20',
+        primary: '#2C3E76',
       },
     },
   },

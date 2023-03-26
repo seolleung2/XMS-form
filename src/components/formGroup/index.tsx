3ff8a78e210cfb3e73e5ec976b210c3eaae67f6f
seolleung2 <seolleung22@gmail.com>
2023-03-26 22:40:18 +0900

    add: 출근지 Input 에 react-daum-postcode 연결, 주소검색 Modal 컴포넌트 생성 - 주소 검색을 위한 Input 생성 및 react-daum-postcode 라이브러리 연동 - readOnly Input 클릭 시 주소 검색 모달 오픈 - 모달 컴포넌트는 React.Portal 을 적용하여 생성

--- src/components/formGroup/index.tsx
+++ src/components/formGroup/index.tsx
@@ -1,8 +1,10 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { FieldValues, useFormContext } from 'react-hook-form';
+import DaumPostcode from 'react-daum-postcode';
 import dayjs from 'dayjs';
 import Input from '@components/input';
 import Button from '@components/button';
+import Modal from '@components/modal';
 import RangePicker from '@components/rangePicker';
 import SelectInput from '@components/selectInput';
 import { ITEM_SELECTION, SUPPLY_SELECTION } from 'constant/form';
@@ -12,11 +14,14 @@ const FormGroup: FunctionComponent = () => {
   const {
     register,
     handleSubmit,
-    setValue,
     watch,
+    clearErrors,
+    setValue,
     formState: { isDirty, errors, isSubmitting },
   } = methods;
 
+  const [openPostcode, setOpenPostcode] = useState<boolean>(false);
+  const [addressInputId, setAddressInputId] = useState<string>('');
   const [itemField, itemDetailField] = watch(['item', 'itemDetail']);
   const [supplyField, supplyDetailField] = watch(['supply', 'supplyDetail']);
 
@@ -49,84 +54,128 @@ const FormGroup: FunctionComponent = () => {
     console.log('body', body);
   };
 
+  const handleOpenPostcodeModal = (addressId: string) => {
+    setAddressInputId(addressId);
+    setOpenPostcode(true);
+  };
+
+  const handleSelectAddress = (address: { roadAddress: string }) => {
+    setValue(addressInputId as any, address.roadAddress);
+    clearErrors(addressInputId as any);
+    setOpenPostcode(false);
+    setAddressInputId('');
+  };
+
+  const handleClosePostcodeModal = () => {
+    setOpenPostcode(false);
+  };
+
   return (
-    <form
-      onSubmit={handleSubmit(async (data) => {
-        await new Promise((r) => setTimeout(r, 1000));
-        try {
-          onSubmit(data);
-        } catch (error) {
-          console.log(error);
-        }
-      })}
-    >
-      <div className="formGroup">
-        <Input
-          label="이름"
-          id="name"
-          className="mb-4"
-          autoFocus
-          maxLength={20}
-          aria-invalid={!isDirty ? undefined : errors.name ? 'true' : 'false'}
-          message={errors['name']?.message as string}
-          {...register('name', {
-            required: '값을 입력 해주세요.',
-            validate: (value) => {
-              const check = /^[가-힣a-zA-Z\s]+$/;
+    <>
+      <form
+        onSubmit={handleSubmit(async (data) => {
+          await new Promise((r) => setTimeout(r, 1000));
+          try {
+            onSubmit(data);
+          } catch (error) {
+            console.log(error);
+          }
+        })}
+      >
+        <div className="formGroup">
+          <Input
+            label="이름"
+            id="name"
+            className="mb-4"
+            autoFocus
+            maxLength={20}
+            aria-invalid={!isDirty ? undefined : errors.name ? 'true' : 'false'}
+            message={errors['name']?.message as string}
+            {...register('name', {
+              required: '값을 입력 해주세요.',
+              validate: (value) => {
+                const check = /^[가-힣a-zA-Z\s]+$/;
 
-              if (!check.test(value))
-                return '한글, 영어, 공백만 입력 가능 합니다.';
-            },
-          })}
-        />
-        <Input
-          label="휴대폰 번호"
-          id="phoneNumber"
-          className="mb-4"
-          maxLength={13}
-          aria-invalid={!isDirty ? undefined : errors.name ? 'true' : 'false'}
-          message={errors['phoneNumber']?.message as string}
-          {...register('phoneNumber', {
-            required: '값을 입력 해주세요.',
-            validate: (value) => {
-              const check = /^([0-9]{3})-?([0-9]{4})-?([0-9]{4})$/;
+                if (!check.test(value))
+                  return '한글, 영어, 공백만 입력 가능 합니다.';
+              },
+            })}
+          />
+          <Input
+            label="휴대폰 번호"
+            id="phoneNumber"
+            className="mb-4"
+            maxLength={13}
+            aria-invalid={!isDirty ? undefined : errors.name ? 'true' : 'false'}
+            message={errors['phoneNumber']?.message as string}
+            {...register('phoneNumber', {
+              required: '값을 입력 해주세요.',
+              validate: (value) => {
+                const check = /^([0-9]{3})-?([0-9]{4})-?([0-9]{4})$/;
 
-              if (!check.test(value))
-                return '알맞는 핸드폰 번호 형식을 입력 해주세요.';
+                if (!check.test(value))
+                  return '알맞는 핸드폰 번호 형식을 입력 해주세요.';
 
-              setValue(
-                'phoneNumber',
-                value
-                  .replace(/^(\d{3})(\d{4})(\d{4})$/g, '$1-$2-$3')
-                  .replace(/-{1,2}$/g, '')
-              );
-            },
-          })}
-        />
-        <RangePicker label="날짜" className="mb-4" />
-        <SelectInput
-          label="품목"
-          className="mb-4"
-          item="item"
-          itemDetail="itemDetail"
-          itemField={itemField}
-          itemDetailField={itemDetailField}
-          options={ITEM_SELECTION}
-        />
-        <SelectInput
-          label="물량"
-          className="mb-4"
-          item="supply"
-          itemDetail="supplyDetail"
-          itemField={supplyField}
-          itemDetailField={supplyDetailField}
-          options={SUPPLY_SELECTION}
-        />
-      </div>
-      <div className="addButton mt-1.5">
-        <Button text="등록" className="" />
-      </div>
-    </form>
+                setValue(
+                  'phoneNumber',
+                  value
+                    .replace(/^(\d{3})(\d{4})(\d{4})$/g, '$1-$2-$3')
+                    .replace(/-{1,2}$/g, '')
+                );
+              },
+            })}
+          />
+          <RangePicker label="날짜" className="mb-4" />
+          <SelectInput
+            label="품목"
+            className="mb-4"
+            item="item"
+            itemDetail="itemDetail"
+            itemField={itemField}
+            itemDetailField={itemDetailField}
+            options={ITEM_SELECTION}
+          />
+          <SelectInput
+            label="물량"
+            className="mb-4"
+            item="supply"
+            itemDetail="supplyDetail"
+            itemField={supplyField}
+            itemDetailField={supplyDetailField}
+            options={SUPPLY_SELECTION}
+          />
+          <Input
+            label="출근지"
+            id="address"
+            className="mb-4"
+            readOnly
+            aria-invalid={
+              !isDirty ? undefined : errors.address ? 'true' : 'false'
+            }
+            message={errors['address']?.message as string}
+            onClick={() => handleOpenPostcodeModal('address')}
+            {...register('address', {
+              required: '값을 입력 해주세요.',
+            })}
+          />
+        </div>
+        <div className="addButton mt-1.5">
+          <Button text="등록" className="" />
+        </div>
+      </form>
+      {openPostcode && (
+        <Modal title="주소 검색" onClose={handleClosePostcodeModal}>
+          <DaumPostcode
+            onComplete={handleSelectAddress}
+            style={{
+              width: '100%',
+              height: 530,
+              padding: '24px',
+            }}
+          />
+        </Modal>
+      )}
+    </>
   );
 };
 
